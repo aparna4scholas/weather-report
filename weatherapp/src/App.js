b54ce6aa4ac3b6b2e2f2ed159733395a4101369c
aparna4scholas <aparna4scholas@gmail.com>
2020-04-09 14:32:50 -0700

    added forecast

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import Weather from './components/Weather';
-
+const apikey = process.env.REACT_APP_API_KEY;
+console.log(apikey);
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -27,7 +28,7 @@ class App extends React.Component {
   // }
 
   getWeatherData() {
-    axios.get('https://api.openweathermap.org/data/2.5/forecast?q=london&appid=e4447104f032368a2a14af1ccf6fd22e&units=imperial')
+    axios.get('https://api.openweathermap.org/data/2.5/forecast?q=london&appid=' + apikey + '&units=imperial')
     .then(response => {
       // console.log(response.data);
       this.setState({ weather: response.data });
@@ -38,7 +39,7 @@ class App extends React.Component {
     if(this.state.weather === null) {
       return <h2>no information</h2>
     } else {
-      this.state.weather.list.map(listItem => <Weather />);
+      this.state.weather.list.map(listItem => <Weather dt={listItem.weather} />);
     }
   }
 
