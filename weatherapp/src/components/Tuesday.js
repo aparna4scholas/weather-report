34d11d53c292ff501cf00bdd3f648f014caa20da
aparna4scholas <aparna4scholas@gmail.com>
2020-04-09 15:21:27 -0700

    modified

---
+++ weatherapp/src/components/Tuesday.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+
+function  Tuesday() {
+   
+        return (
+            <div>
+              <h2> Tuesday</h2>  
+            </div>
+        )
+    
+}
+
+export default Tuesday;
